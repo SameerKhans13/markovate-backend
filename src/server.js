e6ddfcce32a2334db2509f92a737d3b14cc44d32
SameerKhans13 <sam13sameer.13khan@gmail.com>
2024-12-03 06:22:17 +0530

    Implement of studentportal

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { body, validationResult } from "express-validator";
-import { initializeDatabase } from "./config/database.js";
+import { initializeDatabase } from "./config/database.js";  // Ensure the correct path is used
 import dotenv from "dotenv";
-
+import studentportal from "./Examportal/studentportal.js";
 import authRoutes from "./routes/authRoutes.js";
 import dashboardroute from "./routes/dashboardRoutes.js";
 
@@ -15,10 +15,11 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use('/api/answers', studentportal);
 app.use("/auth", authRoutes);
 app.use("/dashboard", dashboardroute);
 
+// Initialize the database
 initializeDatabase();
 
 app.listen(8787, () => {
